d4e9eda31bc1e7afaae08a1bc90c7df952884596
Juan Alberto <japc93k@gmail.com>
2020-09-28 23:22:07 +0200

    Actualizados los ejercicios del modulo 2

--- modulos/2-Lenguajes/ejercicios.ts
+++ modulos/2-Lenguajes/ejercicios.ts
@@ -1,7 +1,7 @@
 /** Head
    Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. 
    Utiliza destructuring. */
-const head = (arr) => {
+const head = (arr: any[]): any => {
   if (arr) {
     const [head] = arr;
     return head;
@@ -12,19 +12,19 @@ const head = (arr) => {
 /** Tail
 Implementa una función tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. 
 Utiliza rest operator. */
-const tail = (arr) => {
+const tail = (arr: any[]): any[] => {
   if (arr) {
     const [, ...tail] = arr;
     return tail;
   }
-  return null;
+  return [];
 };
 
 /** Init 
 Implementa una función init (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. 
 Utiliza los métodos que ofrece Array.prototype. */
 
-const init = (arr) => {
+const init = (arr: any[]): any[] => {
   if (arr) {
     return arr.slice(0, arr.length - 1);
   }
@@ -35,7 +35,7 @@ const init = (arr) => {
 /**Last
 Implementa una función last (inmutable), tal que, dado un array como entrada devuelva el último elemento. */
 
-const last = (arr) => {
+const last = (arr: any[]): any => {
   if (arr) {
     return arr[arr.length - 1];
   }
@@ -45,7 +45,7 @@ const last = (arr) => {
 /**Concat
    Implementa una función concat (inmutable) tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos. 
    Utiliza rest / spread operators. */
-const concat = (arrA, arrB) => {
+const concat = (arrA: any[], arrB: any[]): any[] => {
   return [...arrA, ...arrB];
 };
 
@@ -57,7 +57,7 @@ Implementa una versión del ejercicio anterior donde se acepten múltiples array
 /**    Clone
    Implementa una función clone que, a partir de un objeto de entrada source devuelva un nuevo objeto con las propiedades de source */
 
-const clone = (source) => {
+const clone = (source: object): object => {
   return { ...source };
 };
 
@@ -73,7 +73,7 @@ el resultado de mezclar a sobre b sería:
 merge(a, b); // {name: "Maria", age: 31, married: true, surname: "Ibañez", country: "SPA"}
 TIP: Puedes usar la función "clone" del apartado A. */
 
-const merge = (source, target) => {
+const merge = (source: object, target: object) => {
   return { ...target, ...source };
 };
 
@@ -81,4 +81,41 @@ const merge = (source, target) => {
  * Un libro es un objeto con title como string y isRead como booleano.
  * En caso de no existir el libro devolver false TIP: Existe un método de Array.prototype que te ayudará a buscar según un patrón. */
 
-const isBookRead = (books, titleToSearch) => {};
+type Book = {
+  title: string;
+  isRead: boolean;
+};
+
+const isBookRead = (books: Book[], titleToSearch: string): boolean => {
+  const book = books.find((book) => book.title.includes(titleToSearch));
+
+  if (book != null) {
+    return book.isRead;
+  }
+
+  return false;
+};
+
+class SlotMachine {
+  coins: number;
+
+  constructor() {
+    this.coins = 0;
+  }
+  play(): void {
+    this.coins += 1;
+
+    const tries = [true, true, true];
+
+    const hasWon = tries.reduce(
+      (hasWonPrevious) => hasWonPrevious && Math.random() >= 0.5,
+      true
+    );
+
+    if (hasWon) {
+      console.log(`Congratulations!!!. You won ${this.coins} coins!!`);
+    } else {
+      console.log("Good luck next time!!");
+    }
+  }
+}
